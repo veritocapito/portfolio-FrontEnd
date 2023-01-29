bafeab28863686a8c92d244606b1eb374e1f8539
Veronica Capobianco <veritocapito@gmail.com>
2023-01-29 15:19:31 -0300

    se incorporan validaciones de formulario

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -10,14 +10,51 @@ import { Router } from '@angular/router';
 })
 export class SigninComponent implements OnInit {
 
-  
-  constructor() { 
+  form:FormGroup;
 
+  //Inyectar en el constructor el formBuilder
+  constructor(private formBuilder:FormBuilder) { 
+    //Creamos el grupo de controles para el formulario de login
+    this.form=this.formBuilder.group({
+      password:['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.email]]
+    })
     
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  //metodos para el formulario
+
+  get Password(){
+    return this.form.get("password");
   }
 
+  get Mail(){
+    return this.form.get("email");
+  }
+
+  get PasswordValid(){
+    return this.Password?.touched && !this.Password?.valid;
+  }
   
+  get MailValid(){
+    return this.Mail?.touched && !this.Mail?.valid;
+  }
+  
+
+  onSubmit(event: Event){
+    //detenemos la ejecucion del comportamiento del submit del form
+    event.preventDefault;
+
+    if (this.form.valid){
+      //Llamamos a nuestro servicio para enviar los datos al servidor. 
+      //Tambien podriamos ejecutar alguna logica extra.
+      alert("Todo ok! Enviar formulario!");
+    } else {
+      //Corremos todas las validaciones para que se ejecuten los mensajes de error en el template.
+      this.form.markAllAsTouched();
+    }
+
+  }
 }
